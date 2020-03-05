f351e42e107168eedb23b9bc01bdb70ffe1f62cc
Limon <hslimonffs@outlook.com>
2020-03-05 12:20:07 +0800

    bug fix

--- plot__Kmeans_progress.m
+++ plot__Kmeans_progress.m
@@ -1,7 +1,7 @@
 function plot__Kmeans_progress(X, centroids, bef_centroids, idx, K, i)
   plot_points(X, idx, K);
   plot(centroids(:, 1), centroids(:, 2), 'x', 'MarkerEdgeColor', 'k', ...point
-  'MarkerSize', 9, 'LineWidth', 2);
+  'MarkerSize', 10, 'LineWidth', 3);
   for j = 1:size(centroids, 1)%line
     plot_line(centroids(j, :), bef_centroids(j, :));
   endfor
